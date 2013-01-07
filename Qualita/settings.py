349058103e72c12d939e5dbbf5422f332dfc2c3c
Flavio Marcato <flavius476@gmail.com>
2013-01-07 11:51:11 +0100

    Sviluppo db [core].

--- Qualita/settings.py
+++ Qualita/settings.py
@@ -48,7 +48,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ABSOLUTE_PATH + 'uploaded/'
+MEDIA_ROOT = ABSOLUTE_PATH + 'media/uploaded/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class URL(models.Model):
+    indirizzo = models.URLField()
+    percorso_su_disco = models.FilePathField(path=settings.MEDIA_ROOT)
+    class Meta:
+        verbose_name_plural = "URL"
+    def __unicode__(self):
+        return u'%s' % (self.indirizzo)
+
+class Interrogazione(models.Model):
+    titolo = models.CharField(max_length=50)
+    descrizione = models.TextField(max_length=50)
+    url = models.ManyToManyField(URL)
+    class Meta:
+        verbose_name_plural = "Interrogazioni"
+    def __unicode__(self):
+        return u'%s' % (self.titolo)
