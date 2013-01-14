368cf54a4a499c229af1d0d02a4033adbbf3606a
Flavio Marcato <flavius476@gmail.com>
2013-01-14 11:37:04 +0100

    Avvisi regolarizzati [core].

--- core/views.py
+++ core/views.py
@@ -29,42 +29,45 @@ def render_to_home(request):
 @login_required
 def valutazioni(request, id_interrogazione, current_url=None):
     ValutazioniFormSet = modelformset_factory(Score, form=ValutazioniForm, extra=1, max_num=1)
+    
+    # Raccolta dati e Paginazione
+    page = request.GET.get('page')
+    interrogazione = Interrogazione.objects.get(id=id_interrogazione)                
+    url_list = interrogazione.url.all()
+    paginator = Paginator(url_list, 1)
+    try:
+        url_list = paginator.page(page)
+    except PageNotAnInteger:
+        # Prima pagina
+        url_list = paginator.page(1)
+    except EmptyPage:
+        # Pagina out-of-range
+        url_list = paginator.page(paginator.num_pages) 
+
     if request.method == 'POST': 
         url = get_object_or_404(URL, id=current_url)
         formset = ValutazioniFormSet(request.POST, request.FILES) 
+        ids = []
         if formset.is_valid():
             for form in formset:
                 form = form.save(commit=False)
                 form.url = url
                 form.author = request.user
+                ids.append(form.id)
         formset.save()
-        request.session['msg_ok'] = "Voto inviato con successo!"
-        return HttpResponseRedirect(reverse("valutazioni", args=(id_interrogazione,)))
+        if formset.has_changed():
+            request.session['msg_ok'] = "Voto n. %s inviato con successo!" % ids 
+        return HttpResponseRedirect(reverse("valutazioni", args=(id_interrogazione,)) + "?page=" + page)
     else:
-        # Raccolta dati e paginazione
-        interrogazione = Interrogazione.objects.get(id=id_interrogazione)                
-        url_list = interrogazione.url.all()
-        paginator = Paginator(url_list, 1)
-        page = request.GET.get('page')
-        try:
-            url_list = paginator.page(page)
-        except PageNotAnInteger:
-            # Prima pagina
-            url_list = paginator.page(1)
-        except EmptyPage:
-            # Pagina out-of-range
-            url_list = paginator.page(paginator.num_pages) 
         page_query = Score.objects.filter(id__in = [url.id for url in url_list])
         formset = ValutazioniFormSet(queryset=page_query)
-
         context = {
             'url_list': url_list,
             'formset': formset,
             'id_interrogazione': id_interrogazione,
         }
-
+        # Pulizia sessione
         if request.session.get('msg_ok'):
             context['msg_ok'] = request.session.get('msg_ok')
             del request.session['msg_ok']
-
         return render_to_response('valutazioni.html', RequestContext(request, context))
