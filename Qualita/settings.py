28b8a9be160bb7c5aea9104f8f58061b8c513dc3
Flavio Marcato <flavius476@gmail.com>
2013-06-16 08:37:58 +0000

    Arrangiati link nella barra di navigazione. [core]

--- Qualita/settings.py
+++ Qualita/settings.py
@@ -28,7 +28,8 @@ DATABASES = {
 } 
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+if not DEBUG:
+    DATABASES['default'] =  dj_database_url.config()
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
