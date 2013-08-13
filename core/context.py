7bb440392c6b71de7c5a1f71261f8eb7cdf28216
Flavio Marcato <flavius476@gmail.com>
2013-08-13 09:31:07 +0000

    Aggiunto context processor personalizzato.

---
+++ core/context.py
@@ -0,0 +1,20 @@
+"""
+	Custom context processors for this app
+"""
+
+from datetime import date
+from models import Interrogazione
+
+def copyright(request):
+    START_YEAR = 2013
+    this_year = date.today().year
+    if START_YEAR != this_year:
+        copy_year = "%s - %s" % (START_YEAR, this_year)
+    else:
+    	copy_year = START_YEAR
+    return {'copyright': copy_year}
+
+
+def interrogazioni(request):
+	interrogazioni = Interrogazione.objects.all()
+	return {'interrogazioni': interrogazioni}
